ed4c3748e284481903adf7e179b3394b677e0702
Zulfirman Gherar Fiano <zulfirman098@gmail.com>
2022-06-22 17:01:55 +0700

    ubah url

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -4,7 +4,7 @@ import {useState} from "react";
 
 async function getDataNew() {
     const momentNow = moment();
-    const jadwalSholat = await fetch(process.env.URL_API_MYQURAN + `/sholat/jadwal/1301/${momentNow.format('YYYY/MM/DD')}`);
+    const jadwalSholat = await fetch(process.env.URL_API_MYQURAN + "/v1/sholat/jadwal/1301/" + momentNow.format('YYYY/MM/DD'));
     const alIkhlas = await fetch(process.env.URL_API_BANGHASAN + "/quran/format/json/surat/112/ayat/1-1000");
     const alFalaq = await fetch(process.env.URL_API_BANGHASAN + "/quran/format/json/surat/113/ayat/1-1000");
     const anNashr = await fetch(process.env.URL_API_BANGHASAN + "/quran/format/json/surat/110/ayat/1-1000");
@@ -36,17 +36,19 @@ export default function Index() {
     const [counter, setCounter] = useState(count);
     const increment = (id: number) => {
         // @ts-ignore
-        setCounter({ ...counter, [id]: counter[id] + 1 })
+        setCounter({...counter, [id]: counter[id] + 1})
     };
+
     function countAdd(id: number) {
         // @ts-ignore
-        if(counter[id]>32){
-            setCounter({ ...counter, [id]: 1 })
+        if (counter[id] > 32) {
+            setCounter({...counter, [id]: 1})
             return true;
         }
         // @ts-ignore
-        setCounter({ ...counter, [id]: counter[id] + 1 })
+        setCounter({...counter, [id]: counter[id] + 1})
     }
+
     let i = 1;
     return (
         <div className="main-content">
@@ -59,28 +61,45 @@ export default function Index() {
                                     <p>Astagfirullah 3X</p>
                                 </div>
                                 <div className="col-md-4">
-                                    <p>Allaahumma antas salaamu waminkas salaamu wailaika ya'uudus salaamu fahayyiina rabbanaa bissalaami wa'adkhilnal jannata daarassalaami tabaarakta rabbanaa wata'aalaita yaadzal jalaali wal ikraam.</p>
-                                    <p>أَسْتَغْفِرُ اللهَ (ثَلاَثاً) اللَّهُمَّ أَنْتَ السَّلاَمُ، وَمِنْكَ السَّلاَمُ، تَبَارَكْتَ يَـا ذَا الْجَلاَلِ وَاْلإِكْرَامِ</p>
+                                    <p>Allaahumma antas salaamu waminkas salaamu wailaika ya'uudus salaamu fahayyiina
+                                        rabbanaa bissalaami wa'adkhilnal jannata daarassalaami tabaarakta rabbanaa
+                                        wata'aalaita yaadzal jalaali wal ikraam.</p>
+                                    <p>أَسْتَغْفِرُ اللهَ (ثَلاَثاً) اللَّهُمَّ أَنْتَ السَّلاَمُ، وَمِنْكَ السَّلاَمُ،
+                                        تَبَارَكْتَ يَـا ذَا الْجَلاَلِ وَاْلإِكْرَامِ</p>
                                 </div>
                                 <div className="col-md-4">
-                                    <p>Laailaaha illallah wahdahu la syariika lahu, lahulmulku walahul hamdu wa huwa ‘alaa kulli syai’in Qodir’
+                                    <p>Laailaaha illallah wahdahu la syariika lahu, lahulmulku walahul hamdu wa huwa
+                                        ‘alaa kulli syai’in Qodir’
 
-                                        Allahuma la mani’a lima a’thoita,  wala mu’tia lima mana’ta, wala yanfa’u dzal jaddi minkal jadd</p>
-                                    <p>لاَ إِلَهَ إِلاَّ اللهُ وَحْدَهُ لاَ شَرِيْكَ لَهُ، لَهُ الْمُلْكُ وَلَهُ الْحَمْدُ وَهُوَ عَلَى كُلِّ شَيْءٍ قَدِيْرٌ، اللَّهُمَّ لاَ مَانِعَ لِمَا أَعْطَيْتَ، وَلاَ مُعْطِيَ لِمَا مَنَعْتَ، لاَ يَنْفَعُ ذَا الْجَدِّ مِنْكَ الْجَدُّ</p>
+                                        Allahuma la mani’a lima a’thoita, wala mu’tia lima mana’ta, wala yanfa’u dzal
+                                        jaddi minkal jadd</p>
+                                    <p>لاَ إِلَهَ إِلاَّ اللهُ وَحْدَهُ لاَ شَرِيْكَ لَهُ، لَهُ الْمُلْكُ وَلَهُ
+                                        الْحَمْدُ وَهُوَ عَلَى كُلِّ شَيْءٍ قَدِيْرٌ، اللَّهُمَّ لاَ مَانِعَ لِمَا
+                                        أَعْطَيْتَ، وَلاَ مُعْطِيَ لِمَا مَنَعْتَ، لاَ يَنْفَعُ ذَا الْجَدِّ مِنْكَ
+                                        الْجَدُّ</p>
                                 </div>
                                 <div className={"col-md-12"}>
                                     <div className={"row text-center"}>
                                         <div className="col-md-4">
                                             <h3>Subhanallah</h3>
-                                            <button onClick={() => { countAdd(1) }} type="button" className="btn btn-primary">&nbsp;{counter[1]}&nbsp;</button>
+                                            <button onClick={() => {
+                                                countAdd(1)
+                                            }} type="button"
+                                                    className="btn btn-primary">&nbsp;{counter[1]}&nbsp;</button>
                                         </div>
                                         <div className="col-md-4">
                                             <h3>Alhamdulillah</h3>
-                                            <button onClick={() => { countAdd(2) }} type="button" className="btn btn-primary">&nbsp;{counter[2]}&nbsp;</button>
+                                            <button onClick={() => {
+                                                countAdd(2)
+                                            }} type="button"
+                                                    className="btn btn-primary">&nbsp;{counter[2]}&nbsp;</button>
                                         </div>
                                         <div className="col-md-4 mb-3">
                                             <h3>Allahu Akbar</h3>
-                                            <button onClick={() => { countAdd(3) }} type="button" className="btn btn-primary">&nbsp;{counter[3]}&nbsp;</button>
+                                            <button onClick={() => {
+                                                countAdd(3)
+                                            }} type="button"
+                                                    className="btn btn-primary">&nbsp;{counter[3]}&nbsp;</button>
                                         </div>
                                     </div>
                                 </div>
